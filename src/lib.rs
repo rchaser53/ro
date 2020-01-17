0b3045064e91134ca7bb4e0e329f974569f4508a
rchaser53 <tayoshizawa29@gmail.com>
2020-01-17 21:48:54 +0900

    try to use arg type in proc_macro

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,24 @@ use syn;
 pub fn hello(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemFn);
     let name = &input.sig.ident;
+
+    for input in input.sig.inputs {
+        match input {
+            syn::FnArg::Receiver(_) => {
+                panic!("aa");
+            }
+            syn::FnArg::Typed(t) => {
+                let kind = t.ty.clone();
+                let result = quote! {
+                    fn #name(a: #kind) -> #kind { 42 + a }
+                };
+                return result.into();
+            }
+        };
+    }
+
     let result = quote! {
-        fn #name() -> u32 { 42 }
+        fn #name(a: u32) -> u32 { 42 + a }
     };
     result.into()
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,7 +1,11 @@
 #[ro::hello]
-fn wrapped_function() {}
+fn wrapped_function(a: u64) {}
+
+#[ro::hello]
+fn wrapped_function2(a: u32) {}
 
 #[test]
 fn works() {
-    wrapped_function();
+    assert_eq!(42 + 3, wrapped_function(3));
+    assert_eq!(42 + 1, wrapped_function2(1));
 }
